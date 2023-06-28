2bb9beadb51249aa179bd4c40ca944beda9ccc40
xDepcio <olek.drwal@gmail.com>
2023-06-28 18:26:36 +0200

    fixed blobs again >:(

--- components/Blobs.tsx
+++ components/Blobs.tsx
@@ -2,7 +2,7 @@ import './Blobs.css'
 
 export default function Blobs() {
     return (
-        <div className='overflow-hidden absolute top-0 -z-[1] max-w-[100vw] max-h-[100vh] w-full h-full'>
+        <div className='overflow-hidden absolute top-0 -z-[1] max-w-[100vw] w-full h-full'>
             <div className="blob-1">
 
             </div>
