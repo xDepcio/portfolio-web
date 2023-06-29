8312ded272ef2fa5edd56ec7cb18228df21220b4
xDepcio <olek.drwal@gmail.com>
2023-06-29 11:29:33 +0200

    added scroll up button

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import Navbar from '@/components/Navbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import ScrollUp from '@/components/ScrollUp'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +19,7 @@ export default function RootLayout({
         <html lang="en">
             <body className={inter.className}>
                 <Navbar />
+                <ScrollUp />
                 {children}
             </body>
         </html>
--- components/ScrollUp.tsx
+++ components/ScrollUp.tsx
@@ -1,12 +1,30 @@
 'use client'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './ScrollUp.css'
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons'
+import { useEffect } from 'react'
 
 export default function ScrollUp() {
 
+    useEffect(() => {
+        const scrollUp = document.querySelector('.scroll-up-wrapper')
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > 500) {
+                scrollUp?.classList.add('show')
+                scrollUp?.classList.remove('hide')
+                scrollUp?.classList.remove('initial')
+            }
+            else {
+                scrollUp?.classList.remove('show')
+                scrollUp?.classList.add('hide')
+            }
+        })
+    })
+
 
     return (
-        <div className="scroll-up-wrapper">
-
+        <div onClick={() => ScrollUp()} className="scroll-up-wrapper hide initial">
+            <FontAwesomeIcon className='scroll-arrow' icon={faArrowUp} />
         </div>
     )
 }
