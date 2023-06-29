38ed45000f7ba25b152b455cdf41cd26ba445041
xDepcio <olek.drwal@gmail.com>
2023-06-29 22:11:54 +0200

    added big project pane

---
+++ app/projects/SingleFullProject.tsx
@@ -0,0 +1,49 @@
+import { faMinus, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Image from "next/image";
+import './SingleFullProject.css'
+import { BsCodeSlash } from 'react-icons/bs'
+import { ReactNode } from "react";
+import Link from "next/link";
+
+export default function SingleFullProject({ title, desc, img, inList, missingList }:
+    { title: string, desc: string, img: string, inList: [string, ReactNode][], missingList: [string, ReactNode][] }
+) {
+    return (
+        <div className='projects-page-single-project reveal'>
+            <Image src={'/mc-server-tiles.png'} alt='sdas' width={500} height={200} />
+            <div className="big-project-header-wrapper">
+                <h3 className="big-project-header">{title}</h3>
+                <Link className="code-project" href={'/'}>
+                    see code
+                    <BsCodeSlash />
+                </Link>
+            </div>
+            <p className="big-project-desc">{desc}</p>
+            <h3 className="project-page-sub-header">Whats in?</h3>
+            <ul className="first-ul-projects">
+                {inList.map((item, index) => {
+                    return (
+                        <li key={index}>
+                            <FontAwesomeIcon className="max-w-[0.8rem] text-green-400" icon={faPlus} />
+                            <p>{item[0]}</p>
+                            {item[1]}
+                        </li>
+                    )
+                })}
+            </ul>
+            <h3 className="project-page-sub-header">Whats missing?</h3>
+            <ul>
+                {missingList.map((item, index) => {
+                    return (
+                        <li key={index}>
+                            <FontAwesomeIcon className="max-w-[0.8rem] text-red-400" icon={faMinus} />
+                            <p>{item[0]}</p>
+                            {item[1]}
+                        </li>
+                    )
+                })}
+            </ul>
+        </div >
+    )
+}
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,7 +1,75 @@
+import Image from 'next/image'
+import './ProjectsPage.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDiagramNext, faPhone } from '@fortawesome/free-solid-svg-icons'
+import { faReact } from '@fortawesome/free-brands-svg-icons'
+import SingleFullProject from './SingleFullProject'
+import { SiNextdotjs, SiSequelize, SiExpress } from 'react-icons/si'
+import { BsStripe } from 'react-icons/bs'
+import { BiLogoJavascript, BiLogoNodejs, BiLogoReact, BiPhone } from 'react-icons/bi'
+import { HiOutlineDeviceMobile } from 'react-icons/hi'
+
 export default function ProjectsPage() {
     return (
-        <div>
-            <h1>Projects</h1>
-        </div>
+        <section className='projects-page-wrapper'>
+            <div className='projects-page-inner-wrapper'>
+                <h3 className='projects-page-header reveal'>Personal projects</h3>
+                <div className="projects-page-projects">
+                    <SingleFullProject
+                        desc='dasdsa'
+                        title='Minecraft server webpage'
+                        img='/mc-server-tiles.png'
+                        inList={[
+                            ['JavaScript', <BiLogoJavascript className="text-yellow-400" />],
+                            ['React', <BiLogoReact className="text-blue-400" />],
+                            ['NodeJS', <BiLogoNodejs className="text-green-500" />],
+                            ['Sequelize', <SiSequelize className="text-blue-400" />],
+                            ['Express', <SiExpress />],
+                            ['Stripe API', <BsStripe />]
+                        ]}
+                        missingList={[
+                            ['RWD', <HiOutlineDeviceMobile />],
+                        ]}
+                    />
+                    <SingleFullProject
+                        desc='dasdsa'
+                        title='Minecraft server webpage'
+                        img='/mc-server-tiles.png'
+                        inList={[
+                            ['JavaScript', <BiLogoJavascript />],
+                            ['React', <BiLogoReact />],
+                            ['NodeJS', <BiLogoNodejs />],
+                            ['Sequelize', <SiSequelize />],
+                            ['Express', <SiExpress />],
+                            ['Stripe API', <BsStripe />]
+                        ]}
+                        missingList={[
+                            ['RWD', <HiOutlineDeviceMobile />],
+                        ]}
+                    />
+                    <SingleFullProject
+                        desc='dasdsa'
+                        title='Minecraft server webpage'
+                        img='/mc-server-tiles.png'
+                        inList={[
+                            ['JavaScript', <BiLogoJavascript />],
+                            ['React', <BiLogoReact />],
+                            ['NodeJS', <BiLogoNodejs />],
+                            ['Sequelize', <SiSequelize />],
+                            ['Express', <SiExpress />],
+                            ['Stripe API', <BsStripe />]
+                        ]}
+                        missingList={[
+                            ['RWD', <HiOutlineDeviceMobile />],
+                        ]}
+                    />
+                </div>
+                <h3 className='projects-page-header'>Projects for university</h3>
+                <div className="project-page-projects">
+
+                </div>
+
+            </div>
+        </section>
     )
 }
