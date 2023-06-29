b2e92181f5dd6cbc5261b6ea212d02447c169226
xDepcio <olek.drwal@gmail.com>
2023-06-29 16:58:20 +0200

    adde home navigation button

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,9 @@
 import { Roboto, Roboto_Mono } from 'next/font/google'
 import './Navbar.css'
+import Link from 'next/link'
+import RocketSvg from './resources/RocketSvg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFire, faRocket } from '@fortawesome/free-solid-svg-icons'
 
 const roboto = Roboto({
     weight: ['100', '300', '400', '500', '700', '900'],
@@ -15,9 +19,17 @@ export default function Navbar() {
     return (
         <div className={robotoMono.className + " nav-outer-wrapper"}>
             <div className='nav-inner-wrapper'>
+                <Link href={'/'} className='mr-auto'>
+                    <div className='nav-rocket-wrapper'>
+                        <FontAwesomeIcon className='nav-rocket' icon={faRocket} />
+                        <FontAwesomeIcon className='nav-rocket-fire' icon={faFire} />
+                    </div>
+                </Link>
                 <ul className={robotoMono.className + ' nav-list'}>
                     <li>{'about'}</li>
-                    <li>{'projects'}</li>
+                    <Link href={'/projects'}>
+                        <li>{'projects'}</li>
+                    </Link>
                     <li>{'contact'}</li>
                 </ul>
 
