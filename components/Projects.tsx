a11a3ce247e531767e79cf60f18c04866109719f
xDepcio <olek.drwal@gmail.com>
2023-07-01 22:49:46 +0200

    added content on main page

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -14,33 +14,33 @@ export default function Projects() {
             <p className={robotoMono.className + " projects-tag inner reveal"}>{'<personal projects'}<span>{'>'}</span></p>
             <div className='projects-holder'>
                 <ProjectsPane
-                    description='Minecraft server webiste with React on frontend, NodeJS for backend and with Sequalize for Database.'
-                    imageUrl='/mc-server-5.png'
-                    title='Game server webpage'
+                    description='Minecraft server site with React on frontend, NodeJS on backend with Sequalize for Database and with Stripe for payments.'
+                    imageUrl='/mc/mc-server-tiles-16-9.png'
+                    title='Game server site'
                 />
                 <ProjectsPane
-                    description='Minecraft server webiste with React on frontend, NodeJS for backend and with Sequalize for Database.'
-                    imageUrl='/mc-server-5.png'
-                    title='Game server webpage'
+                    description='Eccomerce site with React on frontend, NodeJS on backend with Sequalize for Database and with Stripe for payments.'
+                    imageUrl='/ecommerce/ecc-tiles-16-9.png'
+                    title='Ecommerce site'
                 />
                 <ProjectsPane
-                    description='Minecraft server webiste with React on frontend, NodeJS for backend and with Sequalize for Database.'
-                    imageUrl='/mc-server-5.png'
-                    title='Game server webpage'
+                    description='Multiplayer tabletop RPG game with React on frontend, NodeJS on backend and with Socket.io for realtime communication.'
+                    imageUrl='/boss-monster/boss-tiles-16-9.png'
+                    title='Tabletop RPG'
                 />
             </div>
             <p className={robotoMono.className + " projects-tag inner reveal"}>{'</personal projects'}<span>{'>'}</span></p>
             <p className={robotoMono.className + " projects-tag inner reveal"}>{'<study related projects'}<span>{'>'}</span></p>
             <div className='projects-holder'>
                 <ProjectsPane
-                    description='Minecraft server webiste with React on frontend, NodeJS for backend and with Sequalize for Database.'
-                    imageUrl='/mc-server-5.png'
-                    title='Game server webpage'
+                    description='Chess app made from scratch in C++ with Qt for GUI. Final project for OOP course.'
+                    imageUrl='/chess/chess-tiles-16-9.png'
+                    title='Chess in C++'
                 />
                 <ProjectsPane
-                    description='Minecraft server webiste with React on frontend, NodeJS for backend and with Sequalize for Database.'
-                    imageUrl='/mc-server-5.png'
-                    title='Game server webpage'
+                    description='Flashcards app made from scratch in Python with PyQt for GUI.'
+                    imageUrl='/flashcards/flashcards-tiles-16-9.png'
+                    title='Flashcards in Python'
                 />
                 <ProjectsPane
                     description='Minecraft server webiste with React on frontend, NodeJS for backend and with Sequalize for Database.'
