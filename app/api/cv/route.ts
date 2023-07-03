651e5206dd11fbfeb59fb92d17325e29809a9d4c
xDepcio <olek.drwal@gmail.com>
2023-07-03 03:31:19 +0200

    updated CV

--- app/api/cv/route.ts
+++ app/api/cv/route.ts
@@ -1,39 +1,14 @@
-import path from "path";
-import fs from 'fs/promises'
-import { NextResponse } from "next/server";
-
 // Get CV
 export async function GET() {
-    // // const pdfPath = path.resolve('./public/cv.pdf'); // Replace 'my-pdf-file.pdf' with the path to your PDF file
-    // // const file = fs.createReadStream(pdfPath);
-    // // const blob = await fs.readFile(pdfPath)
-    // const respone = await fetch('https://drive.google.com/u/0/uc?id=14jvyq0MXB66YowtiRt9TnqgLg9Rfq9Vn&export=download', { cache: "no-store" })
-    // const data = await respone.blob()
-    // const text = await data.text()
-    // // const text = blob.toString("utf-8")
-    // // const text2 = 'dasdasda'
-    // return new Response(text, {
-    //     headers: {
-    //         ...respone.headers,
-    //         "content-disposition": `attachment; filename="cv.pdf"`,
-    //         "content-type": "application/pdf",
-    //     }
-    // })
-    // // return NextResponse.json({ message: r })
-
-    // filename for the file that the user is trying to download
-    // const filename = params.filename;
-
-    // external file URL
     const PDF_URL =
-        "https://drive.google.com/u/0/uc?id=14jvyq0MXB66YowtiRt9TnqgLg9Rfq9Vn&export=download";
+        "https://drive.google.com/u/0/uc?id=1oksCSMv4vluXljJjTnugEg0iYKeNAVkL&export=download";
 
     const response = await fetch(PDF_URL);
 
     return new Response(response.body, {
         headers: {
-            ...response.headers, // copy the previous headers
-            "content-disposition": `attachment; filename="${'cv.pdf'}"`,
+            ...response.headers,
+            "content-disposition": `attachment; filename="${'Aleksander-Drwal-CV-03.07.2023.pdf'}"`,
         },
     });
 }
