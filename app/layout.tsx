1ed73be5705df9cc38116c38771b17d9f99d3e66
xDepcio <olek.drwal@gmail.com>
2023-08-31 00:13:30 +0200

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import Footer from '@/components/Footer'
 import { Toaster } from 'react-hot-toast'
 import Blobs from '@/components/Blobs'
 import NextTopLoader from 'nextjs-toploader';
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -29,6 +30,7 @@ export default function RootLayout({
                 <Navbar />
                 <ScrollUp />
                 <FadeAnimationHandle />
+                <Analytics />
                 {children}
                 <Footer />
             </body>
